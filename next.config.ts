b43f97830538c45a04a8dbd9b0f0f9107415c6b8
Lonejyc <jocelyn.marcilloux@gmail.com>
2026-04-03 14:51:02 +0200

    fix: supprimer transpilePackages, inutile avec R3F v9 + React 19

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   // Output standalone pour Docker (inclut uniquement les fichiers nécessaires)
   output: 'standalone',
 
-  // Nécessaire pour que R3F partage la même instance React que l'app (webpack)
-  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei', '@react-three/postprocessing', 'postprocessing'],
-
   // Augmenter la taille max des réponses pour les gros fichiers
   // Note : la limite d'upload est gérée par le proxy Nginx (client_max_body_size 500M dans Coolify)
   experimental: {
